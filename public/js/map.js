862688006df170ec04d1beb5ee7adc4dd71e3a7a
michaelmhyu1013 <michaelmhyu94@gmail.com>
2020-03-22 18:31:22 -0700

    Add markers for locations

--- public/js/map.js
+++ public/js/map.js
@@ -48,12 +48,17 @@ const segregateDataForEachUser = data => {
         pathMaps.get(locationInfo.userName).push({
             lat: locationInfo.userLatitude,
             lng: locationInfo.userLongitude,
+            time: locationInfo.timeStamp,
+            user: locationInfo.userName,
         });
     });
     return pathMaps;
 };
 
 const addUserPathToMap = (map, path) => {
+    path.forEach(location => {
+        createMarker(map, location);
+    });
     const userPath = new google.maps.Polyline({
         path: path,
         strokeColor: "#" + ((Math.random() * 0xffffff) << 0).toString(16), // generate random hex color
@@ -61,4 +66,24 @@ const addUserPathToMap = (map, path) => {
         strokeWeight: 2,
     });
     userPath.setMap(map);
+};
+
+const createMarker = (map, location) => {
+    console.log(location);
+    const info =
+        `<div><h3>User: ${location.user}</h3>` +
+        `<br>Timestamp: ${location.time}` +
+        `<br>Latitude: ${location.lat}` +
+        `<br>Longitude: ${location.lng}</div>`;
+
+    const marker = new google.maps.Marker({
+        position: new google.maps.LatLng(location.lat, location.lng),
+    });
+    const infowindow = new google.maps.InfoWindow({
+        content: info,
+    });
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
+    });
+    marker.setMap(map);
 };
