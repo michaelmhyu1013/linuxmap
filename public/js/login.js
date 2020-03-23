6eae883185505f9511629887b19927bf3ce12725
michaelmhyu1013 <michaelmhyu94@gmail.com>
2020-03-22 22:18:10 -0700

    Add comments

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,29 @@
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: handleLogin
+--
+-- DATE: March 20, 2020
+--
+-- REVISIONS: N/A
+--
+-- DESIGNER: Michael Yu
+--
+-- PROGRAMMER: Michael Yu
+--
+-- INTERFACE:       handleLogin()
+--
+-- RETURNS: void.
+-- 
+-- NOTES:
+-- Opens a simple prompt in the web browser to prompt for user authentication. If the user fails to pass in the correct
+-- credentials, the loading of the window will be stopped and no resources will be shown to the user.
+----------------------------------------------------------------------------------------------------------------------*/
+const user = "phoenix";
+const pwd = "password";
+
 const handleLogin = () => {
     const username = prompt("Username:", "");
     const password = prompt("Password:", "");
-    if (username !== "phoenix" || password !== "password") {
+    if (username !== user || password !== pwd) {
         window.stop();
     }
 };
--- public/js/map.js
+++ public/js/map.js
@@ -9,15 +9,51 @@ const options = {
     center: vancouver,
 };
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: initMap
+--
+-- DATE: March 16, 2020
+--
+-- REVISIONS: N/A
+--
+-- DESIGNER: Michael Yu
+--
+-- PROGRAMMER: Michael Yu
+--
+-- INTERFACE:       initMap()
+--
+-- RETURNS: void.
+-- 
+-- NOTES:
+-- Creates a Google Map using the Google Maps API and updates the locations on the map using the data fetched from 
+-- connected android clients.
+----------------------------------------------------------------------------------------------------------------------*/
 var initMap = () => {
     gmap = new google.maps.Map(mapDiv, options);
     updateMap(gmap);
 };
 
-const refreshMap = () => {
-    initMap();
-};
-
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: updateMap
+--
+-- DATE: March 16, 2020
+--
+-- REVISIONS: N/A
+--
+-- DESIGNER: Michael Yu
+--
+-- PROGRAMMER: Michael Yu
+--
+-- INTERFACE:       updateMap(map)
+--                      map: Google Map object that will be updated with user locations
+--
+-- RETURNS: void.
+-- 
+-- NOTES:
+-- Call this function to take the specified Google Map and update it with user locations. The user locations is fetched
+-- from a JSON file that contains an array of all locations that are received from any Android clients that are connected
+-- to the node server. Locations that have the same username will be drawn as one polyline on the map.
+----------------------------------------------------------------------------------------------------------------------*/
 const updateMap = map => {
     fetch(resource)
         .then(res => {
@@ -39,6 +75,27 @@ const updateMap = map => {
         });
 };
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: segregateDataForEachUser
+--
+-- DATE: March 16, 2020
+--
+-- REVISIONS: N/A
+--
+-- DESIGNER: Michael Yu
+--
+-- PROGRAMMER: Michael Yu
+--
+-- INTERFACE:      segregateDataForEachUser(data)
+--                      data: JSON array containing user locations
+--
+-- RETURNS: a Map containing all the locations goruped by the user name
+-- 
+-- NOTES:
+-- Call this function to take the specified Google Map and update it with user locations. The user locations is fetched
+-- from a JSON file that contains an array of all locations that are received from any Android clients that are connected
+-- to the node server. Locations that have the same username will be drawn as one polyline on the map.
+----------------------------------------------------------------------------------------------------------------------*/
 const segregateDataForEachUser = data => {
     let pathMaps = new Map();
     data.forEach(locationInfo => {
@@ -55,6 +112,28 @@ const segregateDataForEachUser = data => {
     return pathMaps;
 };
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: addUserPathToMap
+--
+-- DATE: March 16, 2020
+--
+-- REVISIONS: N/A
+--
+-- DESIGNER: Michael Yu
+--
+-- PROGRAMMER: Michael Yu
+--
+-- INTERFACE:      addUserPathToMap(map, path)
+--                      map: Google Map that will have the polylines drawn on it
+--                      path: array containing the locations that will be used to draw the polyline onto the map
+--
+--
+-- RETURNS: void.
+-- 
+-- NOTES:
+-- Call this function to take an array of locations for a user and create a polyline for it. This polyline will then
+-- be set onto the map that is passed in.
+----------------------------------------------------------------------------------------------------------------------*/
 const addUserPathToMap = (map, path) => {
     path.forEach(location => {
         createMarker(map, location);
@@ -68,8 +147,30 @@ const addUserPathToMap = (map, path) => {
     userPath.setMap(map);
 };
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: createMarker
+--
+-- DATE: March 16, 2020
+--
+-- REVISIONS: N/A
+--
+-- DESIGNER: Michael Yu
+--
+-- PROGRAMMER: Michael Yu
+--
+-- INTERFACE:      createMarker(map, location)
+--                      map: Google Map that will have the markers added 
+--                      location: a set of lat and lng values that will be changed 
+--
+--
+-- RETURNS: void.
+-- 
+-- NOTES:
+-- Call this function to create a google map Marker object from the specified location object containing the latitude
+-- and longitude. The marker will then be drawn to the map and an InfoWindow containing the information of the location.
+-- The fields described will be the username, the latitude, the longitude, and the timestamp for the location.
+----------------------------------------------------------------------------------------------------------------------*/
 const createMarker = (map, location) => {
-    console.log(location);
     const info =
         `<div><h3>User: ${location.user}</h3>` +
         `<br>Timestamp: ${location.time}` +
@@ -86,4 +187,26 @@ const createMarker = (map, location) => {
         infowindow.open(map, marker);
     });
     marker.setMap(map);
+};
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: refreshMap
+--
+-- DATE: March 16, 2020
+--
+-- REVISIONS: N/A
+--
+-- DESIGNER: Michael Yu
+--
+-- PROGRAMMER: Michael Yu
+--
+-- INTERFACE:       refreshMap()
+--
+-- RETURNS: void.
+-- 
+-- NOTES:
+-- Refreshes a Goole Map instance by redrawing it.
+----------------------------------------------------------------------------------------------------------------------*/
+const refreshMap = () => {
+    initMap();
 };
